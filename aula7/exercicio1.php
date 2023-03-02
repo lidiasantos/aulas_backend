891aac6a7d849549914e5c73a8924ddfdf752c16
LÃ­dia Maria dos Santos <14027808690@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-02 11:12:38 -0300

    ecercicio1

---
+++ aula7/exercicio1.php
@@ -0,0 +1,60 @@
+<h2>Exercício 1</h2>
+<p>
+    Crie um vetor, como representado abaixo:
+    <pre>
+         --------- 
+        |Murilo   | 
+         --------- 
+        |4        |
+         --------- 
+        |3        |
+         --------- 
+    </pre>
+    <p>
+        Após calcule a média das posições 1 e 2 do vetor e armazene o resultado na posição 3.
+    </p>
+    <p>
+        Utilize uma estrutura de condição verificar se a média,
+ armazenada na posição 3 é maior ou igual a 6, se for armazena 
+ na posição 4 do vetor a situação "Aprovado",caso contrário armazene 
+ "reprovado".
+    </p>
+ <pre>
+         --------- 
+        |Murilo   | 
+         --------- 
+        |4        |
+         --------- 
+        |3        |
+         --------- 
+        |3.5      |
+         --------- 
+        |Reprovado|
+         --------- 
+    </pre>
+
+    <p>
+        Imprima o vetor utilizando o comando var_dump
+    </p>
+    
+</p>
+
+
+<?php
+    $notas = [];
+    $notas[]="Murilo";
+    $notas[] = 4;
+    $notas[] = 3;
+
+    var_dump($notas);
+
+    $notas[] = ($notas[1] + $notas[2]) / 2;
+        if ($notas[3] > 6){
+            $notas[]= "Aprovado";
+        } else{
+            $notas[] = "Reprovado";
+        }
+        var_dump($notas)
+?>
+
+     
