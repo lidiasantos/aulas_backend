bc8591f42c12a5fceabb95ef34b5ba96bc708e08
LÃ­dia Maria dos Santos <14027808690@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-16 11:28:47 -0300

    aula 11

---
+++ aula11/alunos.php
@@ -0,0 +1,29 @@
+<?php 
+    $produto1 = [
+        "nome" => "Poliana Silveira",
+        "nota1" => 7,
+        "nota2" => 9
+    ];
+    $produto2 = [
+        "nome" => "Murilo Alexandre",
+        "nota1" => 8,
+        "nota2" => 5
+    ];
+    $produto3 = [
+        "nome" => "Andre Felipe",
+        "nota1" => 6,
+        "nota2" => 7
+    ];
+    $produto4 = [
+        "nome" => "Lavinia Sandy",
+        "nota1" => 6,
+        "nota2" => 7
+    ];
+    $dados = [
+      $produto1,
+      $produto2,
+      $produto3,
+      $produto4
+    ];
+    echo json_encode($dados);
+    ?>
---
+++ fatima/exemplos/exemplos.js
@@ -0,0 +1,39 @@
+var x = 2;
+var y = 5;
+var w = "Bom dia!";
+var z = 4.5;
+var lista= ["laranja", "maçã", "abacate"];
+
+var soma = x + y;
+var sub = x - y;
+var mult = x * y;
+var div = x / y;
+
+console.log(soma);
+console.log(sub);
+console.log(mult);
+console.log(div);
+
+function exif (){
+if (x % 2 == 0){
+    console.log("número par");
+    }
+    else if {
+        console.log("número ímpar");
+    }else{
+        console.log(w+"não é um valor numerico!")
+    }
+
+for (i; i <=5; i++){
+    console.log(i);
+}
+}          
+var i= 0;
+while (i <=10){
+    console.log("i = " +i);
+    i++;
+}
+for (i in lista){
+    console.log (i);
+    console.log(lista[i]);
+}
